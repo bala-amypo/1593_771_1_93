e609e320ecf343dee53960d59a01167a057b9413
bala-amypo <bala@amypo.in>
2025-12-15 10:17:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,8 @@ public class AuthController {
     public void create(){
 
     }
-    @GetMapping("/login"){
+    @PostMapping("/login")
+    public void login(){
 
     }
 }
