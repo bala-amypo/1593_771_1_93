44c8072e34c9d2f6f4eb5709ab8611a32ec77424
bala-amypo <bala@amypo.in>
2025-12-17 08:52:46 +0000

    User commit

--- src/main/java/com/example/demo/util/JwtUtil.java
+++ src/main/java/com/example/demo/util/JwtUtil.java
@@ -37,6 +37,34 @@
 //     }
 // }
 
+// package com.example.demo.util;
+
+// import io.jsonwebtoken.Claims;
+// import io.jsonwebtoken.Jwts;
+// import io.jsonwebtoken.SignatureAlgorithm;
+
+// import java.util.Date;
+
+// public class JwtUtil {
+
+//     private static final String SECRET_KEY = "secret123";
+
+//     public static String generateToken(String username) {
+//         return Jwts.builder()
+//                 .setSubject(username)
+//                 .setIssuedAt(new Date())
+//                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+//                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+//                 .compact();
+//     }
+
+//     public static Claims extractClaims(String token) {
+//         return Jwts.parser()
+//                 .setSigningKey(SECRET_KEY)
+//                 .parseClaimsJws(token)
+//                 .getBody();
+//     }
+// }
 package com.example.demo.util;
 
 import io.jsonwebtoken.Claims;
@@ -44,14 +72,21 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JwtUtil {
 
     private static final String SECRET_KEY = "secret123";
 
-    public static String generateToken(String username) {
+    public static String generateToken(String email, String role) {
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+
         return Jwts.builder()
-                .setSubject(username)
+                .setClaims(claims)
+                .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
