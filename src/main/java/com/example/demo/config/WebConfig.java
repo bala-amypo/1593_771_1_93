8c29444ea063e8e1cb9a9c7905c31eb8aa0ecbfd
bala-amypo <bala@amypo.in>
2025-12-17 04:30:43 +0000

    User commit

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,4 +1,3 @@
-
 utility
 package com.example.demo.util;
 
@@ -38,27 +37,3 @@ public class JwtUtil {
                 .getBody();
     }
 }
-Servlet
-
-package com.example.demo.servlet;
-
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
-import java.io.IOException;
-
-@WebServlet("/hello-servlet")
-public class SimpleHelloServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(
-            HttpServletRequest request,
-            HttpServletResponse response)
-            throws IOException {
-
-        response.setStatus(200);
-        response.setContentType("text/plain");
-        response.getWriter()
-                .write("Hello from Simple Hello Servlet");
-    }
-}
-
