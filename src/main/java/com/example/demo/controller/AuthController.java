ea291661606e430b6f7550cc4f078f47f36fb33b
bala-amypo <bala@amypo.in>
2025-12-17 04:10:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,18 +1,39 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.util.JwtUtil;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    @PostMapping("/register")
-    public void create(){
 
+    private final UserRepository repo;
+    private final BCryptPasswordEncoder encoder =
+            new BCryptPasswordEncoder();
+
+    public AuthController(UserRepository repo) {
+        this.repo = repo;
     }
-    @PostMapping("/login")
-    public void login(){
 
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setRole("USER");
+        return repo.save(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User request) {
+        User dbUser = repo.findByEmail(request.getEmail());
+        return JwtUtil.generateToken(
+                dbUser.getEmail(), dbUser.getRole());
     }
 }
