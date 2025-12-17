aad29bcc7a8c3d56ccef390167ea9d516b72d540
bala-amypo <bala@amypo.in>
2025-12-17 04:25:42 +0000

    User commit

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -61,30 +61,4 @@ public class SimpleHelloServlet extends HttpServlet {
                 .write("Hello from Simple Hello Servlet");
     }
 }
-Security config
 
-package com.example.demo.config;
-
-import org.springframework.context.annotation.*;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http)
-            throws Exception {
-
-        http.csrf().disable()
-            .authorizeHttpRequests()
-            .requestMatchers(
-                "/auth/**",
-                "/hello-servlet",
-                "/swagger-ui/**"
-            ).permitAll()
-            .anyRequest().authenticated();
-
-        return http.build();
-    }
-}
