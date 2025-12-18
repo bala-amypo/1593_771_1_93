e21db92e035f2c3425994bc3556fcbbeefa9d8f4
bala-amypo <bala@amypo.in>
2025-12-18 09:42:36 +0000

    User commit

--- src/main/java/com/example/demo/model/ActiveIngredient.java
+++ src/main/java/com/example/demo/model/ActiveIngredient.java
@@ -1,42 +1,42 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// import jakarta.persistence.*;
+import jakarta.persistence.*;
 
-// @Entity
-// @Table(
-//     name = "active_ingredients",
-//     uniqueConstraints = @UniqueConstraint(columnNames = "name")
-// )
-// public class ActiveIngredient {
+@Entity
+@Table(
+    name = "active_ingredients",
+    uniqueConstraints = @UniqueConstraint(columnNames = "name")
+)
+public class ActiveIngredient {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//     @Column(nullable = false, unique = true)
-//     private String name;
+    @Column(nullable = false, unique = true)
+    private String name;
 
-//     public ActiveIngredient() {
-//     }
+    public ActiveIngredient() {
+    }
 
-//     public ActiveIngredient(String name) {
-//         this.name = name;
-//     }
+    public ActiveIngredient(String name) {
+        this.name = name;
+    }
 
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public String getName() {
-//         return name;
-//     }
+    public String getName() {
+        return name;
+    }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-//     public void setName(String name) {
-//         this.name = name;
-//     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-// }
+}
--- src/main/java/com/example/demo/model/Medication.java
+++ src/main/java/com/example/demo/model/Medication.java
@@ -1,64 +1,64 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// import jakarta.persistence.*;
-// import java.util.HashSet;
-// import java.util.Set;
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
-// @Entity
-// @Table(name = "medications")
-// public class Medication {
+@Entity
+@Table(name = "medications")
+public class Medication {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//     private String name;
+    private String name;
 
-//     @ManyToMany
-//     @JoinTable(
-//         name = "medication_ingredients",
-//         joinColumns = @JoinColumn(name = "medication_id"),
-//         inverseJoinColumns = @JoinColumn(name = "ingredient_id")
-//     )
-//     private Set<ActiveIngredient> ingredients = new HashSet<>();
+    @ManyToMany
+    @JoinTable(
+        name = "medication_ingredients",
+        joinColumns = @JoinColumn(name = "medication_id"),
+        inverseJoinColumns = @JoinColumn(name = "ingredient_id")
+    )
+    private Set<ActiveIngredient> ingredients = new HashSet<>();
 
-//     public Medication() {
-//     }
+    public Medication() {
+    }
 
-//     public Medication(String name) {
-//         this.name = name;
-//         this.ingredients = new HashSet<>();
-//     }
-//     public void addIngredient(ActiveIngredient ingredient) {
-//         this.ingredients.add(ingredient);
-//     }
+    public Medication(String name) {
+        this.name = name;
+        this.ingredients = new HashSet<>();
+    }
+    public void addIngredient(ActiveIngredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
 
-//     public void removeIngredient(ActiveIngredient ingredient) {
-//         this.ingredients.remove(ingredient);
-//     }
+    public void removeIngredient(ActiveIngredient ingredient) {
+        this.ingredients.remove(ingredient);
+    }
 
-//     public long getId() {
-//         return id;
-//     }
+    public long getId() {
+        return id;
+    }
 
-//     public void setId(long id) {
-//         this.id = id;
-//     }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-//     public String getName() {
-//         return name;
-//     }
+    public String getName() {
+        return name;
+    }
 
-//     public void setName(String name) {
-//         this.name = name;
-//     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-//     public String getIngredient() {
-//         return ingredient;
-//     }
+    public String getIngredient() {
+        return ingredient;
+    }
 
-//     public void setIngredient(String ingredient) {
-//         this.ingredient = ingredient;
-//     }
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
 
-// }
+}
