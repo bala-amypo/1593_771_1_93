0ee69ecdd6c8e91a631c06dbf839456b4bdeef8a
bala-amypo <bala@amypo.in>
2025-12-18 06:03:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
-import com.example.demo.util.JwtUtil;
 
 @RestController
 @RequestMapping("/auth")
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.demo.service.impl;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
