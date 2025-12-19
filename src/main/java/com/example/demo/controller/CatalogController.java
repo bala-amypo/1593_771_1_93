dddc4491e271a113d7dce7a4f6d766dcb0b50da6
bala-amypo <bala@amypo.in>
2025-12-19 13:56:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/CatalogController.java
+++ src/main/java/com/example/demo/controller/CatalogController.java
@@ -1,3 +1,17 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.ActiveIngredient;
+import com.example.demo.model.Medication;
+import com.example.demo.service.CatalogService;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/catalog")
 public class CatalogController {
