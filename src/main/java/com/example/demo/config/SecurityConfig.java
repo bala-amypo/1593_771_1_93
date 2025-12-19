0baffb2b0930af42344d3823a40814f171e4b9d7
bala-amypo <bala@amypo.in>
2025-12-19 06:27:49 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,30 +1,30 @@
-// package com.example.demo.config;
-// import org.springframework.security.web.context.SecurityContextHolderFilter;
-// import org.springframework.context.annotation.*;
-// import org.springframework.security.web.*;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+package com.example.demo.config;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
+import org.springframework.context.annotation.*;
+import org.springframework.security.web.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-// @Configuration
-// public class SecurityConfig {
+@Configuration
+public class SecurityConfig {
 
-//     @Bean
-//     SecurityFilterChain filterChain(HttpSecurity http)
-//             throws Exception {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http)
+            throws Exception {
 
-//         http.csrf().disable()
-//             .authorizeHttpRequests()
-//             .requestMatchers(
-//                 "/auth/**",
-//                 "/hello-servlet",
-//                 "/swagger-ui/**"
-//             ).permitAll()
-//             .anyRequest().authenticated()
-//             .and()
-//             .addFilterBefore(
-//                 new JwtAuthenticationFilter(),
-//                 SecurityContextHolderFilter.class
-//             );
+        http.csrf().disable()
+            .authorizeHttpRequests()
+            .requestMatchers(
+                "/auth/**",
+                "/hello-servlet",
+                "/swagger-ui/**"
+            ).permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(
+                new JwtAuthenticationFilter(),
+                SecurityContextHolderFilter.class
+            );
 
-//         return http.build();
-//     }
-// }
+        return http.build();
+    }
+}
