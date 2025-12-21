e9e30432015d8e4aed05e355bd10e79dd613416c
bala-amypo <bala@amypo.in>
2025-12-21 04:14:13 +0000

    User commit

--- src/main/java/com/example/demo/controller/InteractionController.java
+++ src/main/java/com/example/demo/controller/InteractionController.java
@@ -1,4 +1,4 @@
-package com.example.demo2.controller;
+package com.example.demo.controller;
 
 import java.util.List;
 
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo2.model.InteractionCheckResult;
-import com.example.demo2.service.InteractionService;
+import com.example.demo.model.InteractionCheckResult;
+import com.example.demo.service.InteractionService;
 
 @RestController
 @RequestMapping("/interact")
