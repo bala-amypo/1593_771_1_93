c60c096bc91d87e74481db9b620c513e0bf365b5
bala-amypo <bala@amypo.in>
2025-12-21 05:27:58 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,6 @@
-package com.example.demo2.service;
+package com.example.demo.service;
 
-import com.example.demo2.model.User;
+import com.example.demo.model.User;
 
 public interface UserService {
 
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,11 +1,11 @@
-package com.example.demo2.service.impl;
+package com.example.demo.service.impl;
 
 import org.springframework.stereotype.Service;
 
-import com.example.demo2.exception.ResourceNotFoundException;
-import com.example.demo2.model.User;
-import com.example.demo2.repository.UserRepository;
-import com.example.demo2.service.UserService;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
