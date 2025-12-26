055bb32ffd9e628bdf3cac702c9609ef41400971
bala-amypo <bala@amypo.in>
2025-12-26 06:33:05 +0000

    User commit

--- src/main/java/com/example/demo/dto/IngredientRequestDTO.java
+++ src/main/java/com/example/demo/dto/IngredientRequestDTO.java
@@ -1,44 +1,9 @@
-// package com.example.demo.dto;
-
-// public class IngredientRequest {
-
-//     private String name;
-
-//     public IngredientRequest() {
-//     }
-
-//     public IngredientRequest(String name) {
-//         this.name = name;
-//     }
-
-//     // getter
-//     public String getName() {
-//         return name;
-//     }
-
-//     // setter
-//     public void setName(String name) {
-//         this.name = name;
-//     }
-// }
-
-
-
-
 package com.example.demo.dto;
 
-public class IngredientRequestDTO {
+import lombok.Data;
 
+@Data
+public class IngredientRequestDto {
+    private Long id;
     private String name;
-
-    public IngredientRequestDTO() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/com/example/demo/dto/InteractionCheckRequestDTO.java
+++ src/main/java/com/example/demo/dto/InteractionCheckRequestDTO.java
@@ -1,49 +1,11 @@
-// package com.example.demo.dto;
-
-// import java.util.List;
-
-// public class InteractionCheckRequest {
-
-//     private List<Long> medicationIds;
-
-//     public InteractionCheckRequest() {
-//     }
-
-//     public InteractionCheckRequest(List<Long> medicationIds) {
-//         this.medicationIds = medicationIds;
-//     }
-
-//     // getter
-//     public List<Long> getMedicationIds() {
-//         return medicationIds;
-//     }
-
-//     // setter
-//     public void setMedicationIds(List<Long> medicationIds) {
-//         this.medicationIds = medicationIds;
-//     }
-// }
-
-
-
-
-
 package com.example.demo.dto;
 
+import lombok.Data;
 import java.util.List;
 
-public class InteractionCheckRequestDTO {
-
+@Data
+public class EvaluateRequest {
     private List<Long> medicationIds;
-
-    public InteractionCheckRequestDTO() {
-    }
-
-    public List<Long> getMedicationIds() {
-        return medicationIds;
-    }
-
-    public void setMedicationIds(List<Long> medicationIds) {
-        this.medicationIds = medicationIds;
-    }
+    private List<String> drugCodes; // For SRS compatibility
+    private boolean runAsync = false;
 }
