c3cc245a4a95a15f1c26e21dc2d42b6cbd8685b0
bala-amypo <bala@amypo.in>
2025-12-27 10:28:50 +0000

    User commit

--- src/main/java/com/example/demo/repository/MedicationRepository.java
+++ src/main/java/com/example/demo/repository/MedicationRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.Medication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MedicationRepository extends JpaRepository<Medication, Long> {
+public interface MedicationRepository
+        extends JpaRepository<Medication, Long> {
+
+    boolean existsByName(String name);
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,16 +1,3 @@
-// package com.example.demo.repository;
-
-// import com.example.demo.model.User;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import java.util.Optional;
-
-// public interface UserRepository extends JpaRepository<User, Long> {
-
-//     Optional<User> findByEmail(String email);
-// }
-
-
 package com.example.demo.repository;
 
 import com.example.demo.model.User;
@@ -19,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
