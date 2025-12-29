446d8e9e4bf8abd13ade5474f7a41c2725835035
bala-amypo <bala@amypo.in>
2025-12-29 10:12:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/RuleController.java
+++ src/main/java/com/example/demo/controller/RuleController.java
@@ -1,14 +1,19 @@
 
+
 package com.example.demo.controller;
 
 import com.example.demo.model.InteractionRule;
 import com.example.demo.service.RuleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/rules")
+@Tag(name = "Rules", description = "Manage Drug-to-Drug Interaction Rules")
 public class RuleController {
 
     private final RuleService ruleService;
@@ -18,12 +23,14 @@ public class RuleController {
     }
 
     @PostMapping
-    public InteractionRule addRule(@RequestBody InteractionRule rule) {
-        return ruleService.addRule(rule);
+    @Operation(summary = "Add a new interaction rule")
+    public ResponseEntity<InteractionRule> addRule(@RequestBody InteractionRule rule) {
+        return ResponseEntity.ok(ruleService.addRule(rule));
     }
 
     @GetMapping
-    public List<InteractionRule> listRules() {
-        return List.of();
+    @Operation(summary = "List all interaction rules")
+    public ResponseEntity<List<InteractionRule>> getAllRules() {
+        return ResponseEntity.ok(ruleService.getAllRules());
     }
 }
